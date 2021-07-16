6ece6a6602e58c0fb7ad13d6e82032aa27ce6ae2
ice <zanjarwhite@gmail.com>
2021-07-16 19:30:30 +0600

    added home redirect to /api/

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('courses.urls',
@@ -13,6 +14,7 @@ urlpatterns = [
          'authentication'), namespace='authentication')),
     path('api/profile/', include(('accounts.urls',
          'accounts'), namespace='accounts')),
+    path('', RedirectView.as_view(permanent=False, url='/api/'))
 
 ]
 
